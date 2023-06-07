7d37cb7d25bc0a543b44f86b608aff2741a9d0fe
Bushra Taibani <bushrataibani1@gmail.com>
2023-06-07 15:16:17 +0530

    jobs list

--- src/app/modules/KindJob/Jobs/Jobs.js
+++ src/app/modules/KindJob/Jobs/Jobs.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Paper } from "@mui/material";
 import React, { useEffect } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
